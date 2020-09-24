8ccd6232b83cdd11e867e1beeaad8c7b47c71471
John Davis <johnmdavis125@gmail.com>
2020-09-24 16:35:07 -0400

    updated Albums.js and App.js to display fetched data below form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Albums from './components/Albums';
+import Albums from './components/Albums.js';
 
 function App() {
   return (
     <div className="App">
-      <Albums />
+      <div className="container">
+        <nav>
+          <h4>Add an Album!</h4>
+          <form>
+            <label htmlFor="title">Album Title</label>
+            <input type="text" id="title" />
+            <label htmlFor="artist">Artist Name</label>
+            <input type="text" id="artist" />
+            <label htmlFor="genre">Genre</label>
+            <input type="text" id="genre" />
+            <label htmlFor="img_url">Album Cover url</label>
+            <input type="text" id="img_url" />
+          </form>
+        </nav>
+        <main>
+          <Albums />
+        </main>
+        <aside></aside>
+      </div>
+      <footer />
     </div>
   );
 }
--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,24 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Albums (props) {
+    const [albums, setAlbums] = useState([])
+
     const getAlbums = async () => {
         try {
             const response = await fetch('http://localhost:3000/albums')
             const data = await response.json()
+            setAlbums(data)
             console.log(data)
         } catch(error){
             console.error(error)
         }
     }
         useEffect(() => {
-            (
-                async function (){
+            (async function (){
                     await getAlbums()
                 }
             )()
         }, [])
         
     return (
+    <div>
         <h1>Albums</h1>
+
+        {albums.map( album => {
+            return (
+                <div key={album.id} className="album">
+                    {/* <img src={album.img_url} /> */}
+                    <h3>{ album.title }</h3>
+                    <p>{ album.artist }</p>
+                    <small>{ album.genre }</small>
+                </div>
+            )
+        })}
+
+    </div>
     )
 };
