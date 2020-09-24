d90175981e1e58150aee49ea0dfd14302a4161d4
John Davis <johnmdavis125@gmail.com>
2020-09-24 15:57:06 -0400

    updated Albums.js with getAlbums to ex ajax request from api, added useEffect hook to trigger

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,6 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Albums (props) {
+    const getAlbums = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/albums')
+            const data = await response.json()
+            console.log(data)
+        } catch(error){
+            console.error(error)
+        }
+    }
+        useEffect(() => {
+            (
+                async function (){
+                    await getAlbums()
+                }
+            )()
+        }, [])
+        
     return (
         <h1>Albums</h1>
     )
